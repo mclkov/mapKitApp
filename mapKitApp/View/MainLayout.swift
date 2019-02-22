24aa209e90258cd8103e3088fc6150048653269f
McL <mcl@brm>
2019-02-22 13:04:52 +0200

    project structure

---
+++ mapKitApp/View/MainLayout.swift
@@ -0,0 +1,15 @@
+//
+//  MainLayout.swift
+//  mapKitApp
+//
+//  Created by McL on 2/22/19.
+//  Copyright © 2019 McL. All rights reserved.
+//
+
+import UIKit
+
+extension MainVC {
+    func setupView() {
+        self.view.backgroundColor = .white
+    }
+}
