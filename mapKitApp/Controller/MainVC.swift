716604a2d0dafd5f845cd23881a5022f0acc709f
McL <mcl@brm>
2019-02-22 13:10:13 +0200

    constraint searchBar

--- mapKitApp/Controller/MainVC.swift
+++ mapKitApp/Controller/MainVC.swift
@@ -7,8 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
 class MainVC: UIViewController {
+    let searchBar: UISearchBar = {
+        let uiSearchBar = UISearchBar()
+        return uiSearchBar
+    }()
+    
+    let mapView: MKMapView = {
+        let map = MKMapView()
+        return map
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
--- mapKitApp/View/MainLayout.swift
+++ mapKitApp/View/MainLayout.swift
@@ -10,6 +10,42 @@ import UIKit
 
 extension MainVC {
     func setupView() {
+        setupBackground()
+        setupSearchBar()
+        setupMap()
+    }
+    
+    private func setupBackground() {
         self.view.backgroundColor = .white
+    }
+    
+    private func setupSearchBar() {
+        view.insertSubview(searchBar, at: 0)
+        setupSearchBarAnchors()
+    }
+    
+    private func setupSearchBarAnchors() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+        searchBar.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+    
+    private func setupMap() {
+        view.insertSubview(searchBar, at: 0)
+        setupMapAnchors()
+    }
+    
+    private func setupMapAnchors() {
+        view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        mapView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 0).isActive = true
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
     }
 }
